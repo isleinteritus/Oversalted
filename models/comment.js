cb1fe9449dcb806b6b4db08988b7693189954c70
Philip Gray <pcgrayiii@gmail.com>
2021-12-23 07:43:14 -0800

    Fixed a bracket issue within a model.

--- models/comment.js
+++ models/comment.js
@@ -8,14 +8,18 @@ const commentSchema = new mongoose.Schema(
             type: String,
             required: true
         },
-        commentOwner: {
+        commentOwner: [
+            {
             type: mongoose.Schema.Types.ObjectId,
             ref: "User"
-        },
-        forum: {
+           }
+        ],
+        forum: [
+            {
             type: mongoose.Schema.Types.ObjectId,
             ref: "Forum"
-        }
+            }
+        ],
         createdOn: {
             type: Date,
             default: Date.now
