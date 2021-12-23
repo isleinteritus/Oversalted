b1524ef414fe490dbd458da8704ffdb68208b539
Philip Gray <pcgrayiii@gmail.com>
2021-12-23 14:18:14 -0800

    updated userController  basic route needs. Left notes to add more too.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,108 +1,89 @@
+
 module.exports = function({Model, ViewPath, Router, BooleanKey}) {
-
-
     //ROUTES
     ///////INDEX///////
-    Router.get('/', (req, res) => {
-        //finds all models within Model
-        Model.find({}, (error, allModels) => {
-            if(allModels){
-                //sends the pat to index
-                res.send(`${ViewPath}/Index`, {
-                    [ViewPath]: allModels
-                })
-            }else{
-                console.log('index route:' +error.message)
-            }
-        })
+    Router.get('/', async (req, res)=> {
+        try{
+            //retrieves index of requested Model
+            Model.find({})//Should this pull all from database or limit query? Could frontend handle that?
+            res.send(`${ViewPath}/Index`)
+        }catch(error){
+            res.send({message: error})
+        }
     })
-
     //NEW
+    //creates the endpoint of taking a file path and inputting it before /new. Depending on what user does defines the returned route.
     Router.get('/new', (req, res) => {
         res.send(`${ViewPath}/New`)
     })
 
     //DELETE
-    Router.delete('/:id', (req, res) => {
-        //takes the current model by id and removes it from the collection
-        Model.findByIdAndRemove(req.params.id, (error, deletedModel) =>{
-            if(deletedModel){
-                console.log(deletedModel)
-            }else{
-                console.log('delete route:' + error.message)
-            }
-            res.redirect(`/${ViewPath}`)
-        })
+    Router.delete('/:id', async (req,res) => {
+        try {
+            //finds the model id and removes it from the collection
+            Model.findByIdAndRemove(req.params.id)
+            res.send(`/${ViewPath}`)
+        } catch (error) {
+            res.send({message: error})
+        }
     })
 
     //UPDATE
-    Router.put ('/:id', (req, res) => {
+    Router.put('/:id', async (req,res) => {
         BooleanKey.forEach((key) => {
             req.body[key] = req.body[key] === 'on' ? true : false
         })
-        //update the current document with the model
-        Model.findByIdAndUpdate(
-            {
-                _id: req.params.id
-            },
-            {
-                ...req.body
-            },
-            {
-                new : true
-            },
-            (error, updatedModel) => {
-                if(updatedModel) {
-                    console.log(updatedModel)
-                } else {
-                    console.log('update route:' + error.message)
-                }
-                res.redirect(`/${ViewPath}`)
-            }
-        )
+        try {
+            //updates the document with the new information
+            Model.findByIdAndUpdate(
+                {
+                    _id:req.params.id
+                },
+                {
+                    ...req.body
+                },
+                {
+                    new : true
+                })
+                res.send(`/${ViewPath}/:id`)
+        } catch (error) {
+            res.send({message: error})
+        }
     })
 
     ///////CREATE///////
-    Router.post('/', (req, res) => {
+    Router.post ('/', async (req, res) => {
         BooleanKey.forEach((key) => {
-            req.body[key] = req.body[key] === 'on' ? true : false
+            req.body[key] = req.body[key] === 'on' ? true : false //if false I think it should return an error that use does not have permission.
         })
-        //Creates the Model for t
-        Model.create(req.body, (error, createdModel) => {
-            error ? console.log('create route:' + error.message) : res.redirect(`/${ViewPath}`)
-        })
+        try {
+            Model.create(req.body)
+            res.send(`/${ViewPath}`)
+        } catch (error) {
+            res.send({message: error})
+        }
     })
-
     ///////EDIT///////
-
-    Router.get('/:id/edit', (req, res) => {
-        //Finds the document in the collection
-        Model.findById(req.params.id, (error, foundModel) => {
-            if(error){
-                console.log('edit route:' + error.message)
-            } else {
-            //sends the edit view and passes the found path
-                res.send(`${ViewPath}/Edit`, {
-                    [ViewPath] :foundModel,
-                })
-            }
-        })
+    Router.get('/id/edit', async (req, res) => {
+        //Needs a boolean permission
+        try {
+            //retrieves the id then sends a version that is editable
+            Model.findById(req.params.id)
+            res.send(`${ViewPath}/Edit`)
+        } catch (error) {
+            res.send({message: error})
+        }
     })
 
     ///////SHOW///////
-    Router.get('/:id', (req, res) => {
-        //Find the specific document by id
-        Model.findById(req.params.id, (error, foundModel) => {
-            if(error) {
-                console.log('show route:' + error.message)
-                // res.sendStatus(500) shoots off a false alarm
-            } else {
-                //send the show route and pass it the foundModel
-                res.send(`${ViewPath}/Show`, {
-                    [ViewPath]: foundModel,
-                })
-            }
-        })
+    Router.get('/:id', async (req, res) => {
+        try {
+            //finds specific id and shows it to user
+            Model.findById(req.params.id)
+            res.send(`${ViewPath}/Show`)
+        } catch (error) {
+            res.send({message : error})
+        }
     })
     return Router
 }
