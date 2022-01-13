25b55bdc101a036e85563a47f0c6fad9c67745dc
Philip Gray <pcgrayiii@gmail.com>
2022-01-13 07:31:52 -0800

    user controller routed up. No security measures added.

--- controllers/forums.js
+++ controllers/forums.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
-
+const User = require('../models/user.js')
+const Forum = require('../models/forum.js')
 
     //ROUTES
     ///////INDEX///////
--- controllers/users.js
+++ controllers/users.js
@@ -1,78 +1,87 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../models/user.js')
 
-    //ROUTES
-    ///////INDEX///////
-    router.get('/index', (req, res)=> {
-        try{
-            //retrieves index of requested Model
-            Model.find({})//Should this pull all from database or limit query? find all from back end and only send back what the frontend sends abstract names:'page' 'skip'
-            res.send({message: 'success of something'})
-        }catch(error){
-            res.send({message: error})
-        }
-    })
-
-    //DELETE
-    //todo check if user has permission to delete their account. requires token so ignore this for now on all controllers until ready to implement
-    router.delete('/:id', (req,res) => {
-        try {
-            //finds the model id and removes it from the collection
-            Model.findByIdAndRemove(req.params.id)
-            res.send({message: 'success of something'})
-        } catch (error) {
-            res.send({message: error})
-        }
-    })
-
-    //UPDATE
-    router.put('/:id', (req,res) => {
-        try {
-            //updates the document with the new information
-            Model.findByIdAndUpdate(
-                {
-                    _id:req.params.id
-                },
-                {
-                    ...req.body
-                },
-                {
-                    new : true
-                })
-                res.send({message: 'success of something'})
-        } catch (error) {
-            res.send({message: error})
-        }
-    })
-    ///////CREATE (signup)///////
-    router.post ('/signup', (req, res) => {
-        try {
-            Model.create(req.body)
-            res.send({message: 'success of something'})
-        } catch (error) {
-            res.send({message: error})
-        }
-    })
-    ///////EDIT///////
-    router.get('/:id/Edit', (req, res) => {
-        //placeholder for logic in mind.
-        try {
-            //retrieves the id then sends a version that is editable
-            Model.findById(req.params.id)
-            res.send({message: 'success of something'})
-        } catch (error) {
-            res.send({message: error})
-        }
-    })
-
-    ///////SHOW///////
-    router.get('/:id', (req, res) => {
-        try {
-            //finds specific id and shows it to user
-            Model.findById(req.params.id)
-            res.send({message: 'success of something'})
-        } catch (error) {
-            res.send({message : error})
-        }
-    })
-    module.exports = router
+//ROUTES
+///////CREATE (signup)///////
+router.post('/signup', (req, res) => {
+    //could change req.body to req.body.name/email/password
+        User.create(req.body, (error, createdUser) =>{
+            if (error) {
+                console.error(error)
+            } else {
+                res.json(createdUser)
+            }
+        })
+})
+router.post('/login', (req, res) =>{
+        User.findOne({
+            email: req.body.email,
+            password: req.body.password
+        }, (error, foundUser) =>{
+            if (error) {
+                console.error(error)
+            } else {
+                res.json(foundUser)
+            }
+        })
+})
+///////INDEX///////
+router.get('/index', (req, res)=> {
+        //retrieves index of requested User
+        User.find({
+        }, (error, foundUsers) => {
+            if (error) {
+                console.error(error)
+            } else {
+                res.json(foundUsers)
+            }
+        })//Should this pull all from database or limit query? find all from back end and only send back what the frontend sends abstract names:'page' 'skip'
+})
+///////SHOW///////
+router.get('/:id', (req, res) => {
+        //finds specific id and shows it to user
+        User.findById({
+            _id: req.params.id
+        }, (error, foundUser) => {
+            if (error) {
+                console.error(error)
+            } else {
+                res.json(foundUser)
+            }
+        })
+})
+//UPDATE
+router.put('/:id', (req, res) => {
+        //updates the document with the new information
+        User.findByIdAndUpdate({
+            _id: req.params.id
+        },
+        { //which information should go here?
+            ...req.body
+        },
+        {
+            new: true
+        }, (error, updatedUser) => {
+            if (error) {
+                console.error(error)
+            } else {
+                res.json(updatedUser)
+            }
+        })
+})
+//DELETE
+//todo check if user has permission to delete their account. requires token so ignore this for now on all controllers until ready to implement
+router.delete('/:id', (req,res) => {
+        //finds the User id and removes it from the collection
+        User.findByIdAndRemove({
+            _id: req.params.id
+        }, (error, deletedUser) => {
+            if (error) {
+                console.error(error)
+            } else {
+                res.json(deletedUser)
+            }
+        })
+})
+module.exports = router
