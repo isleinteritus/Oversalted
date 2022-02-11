6feddac7bf3345848a1f2a37c6114134a9fa62b7
Isle Interitus <isleinteritus@gmail.com>
2022-02-11 09:17:33 -0800

    validation added to all needed routes. Note: needs clear error handling through superstruct. Added a ton of todos for reminders for myself to finish later. Everything works as needed for current progress.

---
+++ middlewares/validation.js
@@ -0,0 +1,59 @@
+const { object, string, array, enums, trimmed, size, pattern, nonempty, empty } = require('superstruct')
+
+//TODO enforce role rules? Not sure how to do this yet. Perhaps enforcing a forum owner and comment owner rules for validation? Hm.
+
+//enums = Roles = {
+//     someRole: "some role",
+//     anotherRole: "another role"
+//}
+
+//Validation Schemes\\
+
+//USER SCHEMAS\\
+
+//validation for the registering user
+const regisUserValStruct = object({
+    //few assumptions about how a name /should/ be.
+    //size = type, min, max
+    name: nonempty(size(string(), 1, 30)),
+    email: nonempty(string()),
+    //TODO password confirmation done on frontend
+    //password length min of 8 max of 100
+    password: nonempty(size(string(), 8, 100)),
+    userForums: empty(array()),
+    userComments: empty(array()),
+})
+
+//TODO REFINE
+//validates that this is the user
+const loginUserValStruct = object({
+    email: nonempty(string()),
+    password: nonempty(string()),
+})
+
+//forum schemas\\
+
+//validates the forum submission is correct 
+const postForumValStruct = object({
+    title: nonempty(size(string(), 1, 60)),
+    content: nonempty(string()),
+    forumOwner: nonempty(array()),
+    parentTags: nonempty(array(string()))
+})
+
+//comment schemas\\
+
+//validates that this is the comment
+const postCommentValStruct = object({
+    content: nonempty(string()),
+    commentOwner: nonempty(array()),
+    parentForum: nonempty(array()),
+})
+
+module.exports = {
+    regisUserValStruct,
+    loginUserValStruct,
+    postForumValStruct,
+    postCommentValStruct
+}
+
