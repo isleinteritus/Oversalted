f047f3b02eefd0941a9ac0998815338bc61d1a24
Isle Interitus <isleinteritus@gmail.com>
2022-03-24 10:42:49 -0700

    cleaned up some comment text and shortened it to a simple todo.

--- server.js
+++ server.js
@@ -45,17 +45,7 @@ app.use(express.urlencoded({ extended: true}))
 app.use(methodOverride('_method'))
 app.use(morgan('dev'))
 
-//TODO figure out this more at a later point.
-// //morgan watching the logs
-// morgan.token('sessionid', function(req, res, param) {
-//     return req.sessionID
-// })
-// //TODO add a morgan tracker for when the user logins and generates a new loginkey. This is for debugging purposes and tracking which user had which login key at the time. Loginkey is used in correlation to session management.
-// morgan.token('user', function(req, res, param) {
-//     return req.session.user
-// })
-
-// app.use(morgan('Remote Address| :remote-addr - Remote User| :remote-user Date| [:date[clf]] Method/URL/ ":method :url HTTP/:http-version" Status| :status Response| :res[content-length] Referrer| ":referrer" User Agent| ":user-agent" User| :user SessionID| :sessionid'))
+//TODO a proper logger, most likely something to replace morgan with.
 
 //redis session management
 app.use(redSession)
